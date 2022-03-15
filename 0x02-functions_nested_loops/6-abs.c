b00e6e59501ec11baa7e138f60c2f0a4f53fa86f
OhiaJanny <janechidera12@gmail.com>
2022-03-15 13:00:35 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an interger
+ * @n: int to check
+ * Return: the absolute value of int
+ */
+
+int _abs(int n)
+{
+	 (n > = 0)
+	{
+		return (n);
+	}
+	return (-n);
+}

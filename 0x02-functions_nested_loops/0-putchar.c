b832f1d602ef06f22d33472f133b69d1443e2b1a
OhiaJanny <janechidera12@gmail.com>
2022-03-15 09:18:46 -0700

    y

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
-#include "holterton.h"
+#include "_putchar"
 
 /**
- * main - prints Holberton, followed by a new line
- * Return: Always 0 (success)
+ * main - prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	-putchar('H');
-	-putchar('o');
-	-putchar('l');
-	-putchar('b');
-	-putchar('e');
-	-putchar('r');
-	-putchar('t');
-	-putchar('o');
-	-putchar('n');
-	-putchar('\n');
+	_putchar('_')
+	_putchar('p')
+	_putchar('u')
+	_putchar('t')
+	_putchar('c')
+	_putchar('h')
+	_putchar('a')
+	_putchar('r')
+	_putchar('\r');
 
 	return (0);
-}

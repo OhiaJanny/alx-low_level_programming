58101deb067477ae547940e4d15bd7c195e66626
OhiaJanny <janechidera12@gmail.com>
2022-03-16 07:31:16 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
@@ -30,6 +30,6 @@ void times_table(void)
 				_putchar((k % 10) + '0');
 			}
 		}
-		_putchar('n\');
-	}
+		_putchar('\n');
+
 }

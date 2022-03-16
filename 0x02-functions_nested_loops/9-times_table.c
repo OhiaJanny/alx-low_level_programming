76c3a8463c47f6a2dc52ad47c3fad652292ecff7
OhiaJanny <janechidera12@gmail.com>
2022-03-16 07:51:13 -0700

    l

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,9 +7,9 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0, i < 10, i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0, j < 10, j++)
 		{
 			k = j * i;
 			if (j == 0)
@@ -19,6 +19,7 @@ void times_table(void)
 			if (k < 10 && j != 0)
 			{
 				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
 			} else if (k >= 10)
